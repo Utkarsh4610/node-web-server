7c79b444199ca9a77251bf1ea71c77b48bc14f08
Utkarsh Kumar <utkarsh.kumar4610@gmail.com>
2018-09-23 21:38:43 +0530

    'Initial_Commit'

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    console.log(`${now}: ${req.method} ${req.url}`);
+    next();
+});
+app.use((req,res,next)=>{
+   res.render('maintainence.hbs');
+});
+app.use(express.static(__dirname + '/html'));
+
+hbs.registerHelper('getCurrYear',()=>{
+    return new Date().getFullYear();
+});
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        name:'Uttu'
+    });
+   });
+app.get('/about',(req,res)=>{
+ res.render('about.hbs',{
+     name:'Uttu'
+ });
+});
+
+
+app.get('/bad',(req,res)=>{
+    // res.send('<h1>Hello Express</h1>');
+    res.send({
+        error:'Something went wrong',
+    });
+ });
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000.');
+});
