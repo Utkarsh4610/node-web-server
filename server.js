22a977533e11d3d68f311ec0288fe367c50daed1
Utkarsh Kumar <utkarsh.kumar4610@gmail.com>
2018-09-23 23:54:47 +0530

    'Setup_start_script_and_heroku_env.'

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 var app = express();
+const port = process.env.PORT || 3000;
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine','hbs');
 app.use((req,res,next)=>{
@@ -34,6 +35,6 @@ app.get('/bad',(req,res)=>{
         error:'Something went wrong',
     });
  });
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000.');
+app.listen(port,()=>{
+    console.log(`Server is up on port: ${port}`);
 });
